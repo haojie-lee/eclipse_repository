0e638110f923292e13ffb71cbafd4ee773e1d72d
lihaojie <lihaojie@dongfangfuli.com>
2019-07-26 17:56:28 +0800

    本地仓库新增java工程

---
+++ java_project/src/cn/dffl/bigdata/datbase/operation/overwrite.java
@@ -0,0 +1,5 @@
+package cn.dffl.bigdata.datbase.operation;
+
+public @interface overwrite {
+
+}
---
+++ java_project/src/cn/dffl/bigdata/datbase/operation/test.java
@@ -0,0 +1,22 @@
+package cn.dffl.bigdata.datbase.operation;
+
+import org.junit.Test;
+
+public class test {
+	@Test
+	public void fangfa1() {
+    	System.out.println("hello fangfa1");
+    	
+    }
+	
+	public void fangfa2() {
+    	System.out.println("hello fangfa2");
+    	
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
